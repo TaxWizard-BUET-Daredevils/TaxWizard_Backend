ea34ac9ea4bf9d543433b4b21deaacdc4e5bf442
nayeem-17 <nasa.28apr@gmail.com>
2023-11-03 19:50:46 +0600

    ..

--- run_dev.sh
+++ run_dev.sh
@@ -1,2 +1,8 @@
 #!bin/bash
 uvicorn app.main:app --host=0.0.0.0 --reload 
+
+
+$taskDef = aws ecs describe-task-definition --task-definition tax-w1 --region=eu-west-1 | ConvertFrom-Json
+$taskDef.taskDefinition.containerDefinitions[0].image = "<container>:<version>"
+$containerDefinitions = $taskDef.taskDefinition.containerDefinitions | ConvertTo-Json -Depth 10
+aws ecs register-task-definition --family "tax-w1" --container-definitions $containerDefinitions 
